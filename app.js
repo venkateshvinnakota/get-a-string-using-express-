3a14c1be327e0a4652d165ab5ab50fe2b997412f
venkatesh <venkey.vinnakota@gmail.com>
2022-11-24 05:19:24 +0000

    get a string using express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Express JS");
+});
+
+module.exports = app;
+
+app.listen(3000);
